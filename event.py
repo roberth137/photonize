476765b4f33a52752f062fcb14c26d9280ac7063
Robert Hollmann <rahollmann137@gmail.com>
2024-11-20 00:25:50 +0000

    updated event. some bugs tagging event

--- event.py
+++ event.py
@@ -36,37 +36,43 @@ class binding_event:
         
 def connect_locs(localizations_file):
     localizations = pd.read_hdf(localizations_file, key='locs')
+    #localizations = localizations[:20]
     #grouped_locs = localizations.copy
     #create event array 
     event = np.zeros(len(localizations), dtype=int)
     event_counter = 0
-    deleted = False
-    num_deleted = 0
+    
+    #deleted = False
+    #num_deleted = 0
+    
+    #num_locs_before = len(localizations)
+    
     #loop over all localizations:
     for i in np.arange(len(localizations), dtype=int):
-        print('\n\n----- NEW LOOP -----')
-        print('loop number: ', i, ' starting...')
-        print('deleted localizations: ', num_deleted)
         
-        if deleted:
-            num_deleted += 1
-            event_counter -=1
-            deleted = False
-        position_counter = i - num_deleted
+        print('\n\n--------------- NEW LOOP ---------------')
+        print('loop number: ', i, ' of ', len(localizations)-1, ' starting...\n')
+        #print('deleted localizations: ', num_deleted, '\n')
+        
+        #if deleted:
+        #    num_deleted += 1
+        #    deleted = False
+            
+        #position_counter = i - num_deleted
         
         if i == len(localizations)-1: print('last round')
         print('current event array: ', event[i-2:i+5])
-        has_follower = False
+        #has_follower = False
         
         print('loop number: ', i, ' starting...')
-        print('setting position: ', position_counter , '\n')
+        #print('setting position: ', position_counter , '\n')
         
         if event[i] == 0: #not connected to previous event -> new event
             event_counter +=1 
-            print('updated event counter: ', event_counter)
-            new_event = True 
+            print('--------NEW EVENT:-------- ', event_counter)
+            #new_event = True 
             event[i] = event_counter
-            print('new event. event number: ', event[i])
+            print('event number: ', event[i])
             
         else:
             print('position ', i, ' already set with value: ', event[i], '\n') 
@@ -88,14 +94,28 @@ def connect_locs(localizations_file):
         if has_follower: 
             event[follower_index] = event[i] #set to same event
             print('follower index is: ', follower_index)
-            print('connected to previous. 1new event_arr: ', event[i-2:i+5])
-        if new_event and not has_follower:
-            localizations.drop(index=i)
-            np.delete(event, i)
-            print('deleted standalone localization.')
-            deleted = True
+            print('connected to previous. new event_arr: ', event[i-2:i+5])
+            #new_event = False
+            
+        #print('new event: ', new_event)
+            
+        #if new_event and not has_follower:
+        #    print('new event: ', new_event, '. has follower: ', has_follower)
+        #    new_event = False
+        #    print('length of localizations: ', len(localizations))
+        #    print('--------POP--------')
+        #    localizations = localizations.drop(index=i)
+        #    print('length of localizations: ', len(localizations))
+        #    np.delete(event, i)
+        #    print('deleted standalone localization.')
+        #    deleted = True
         #else: print('no follower nearby')
-        print('----- END LOOP -----')
+                
+        print('--------------- END LOOP ---------------')
+        
+        
+    #print('number of localizations before: ', num_locs_before)
+    print('number of localizations now: ', len(localizations))
     locs_event_tagged = pd.DataFrame({'frame': localizations.frame,
                                  'x': localizations.x, 
                                  'y': localizations.y, 
@@ -114,27 +134,28 @@ def connect_locs(localizations_file):
     
         
 def filter_unique_events(localizations):
-  """Filters a DataFrame, removing localizations 
-  that are not connected to other events.
-
-  Args:
-    localizations: The DataFrame to filter.
-    event_column: The name of the column containing the events to filter.
-
-  Returns:
-    A new DataFrame with the filtered rows.
-  """
-
-  # Extract unique event values and their counts
-  event_counts = localizations['event'].value_counts()
-
-  # Identify events that occur only once
-  unique_events = event_counts[event_counts == 1].index
-
-  # Filter out rows with unique events
-  filtered_locs = localizations[~localizations['event'].isin(unique_events)]
-
-  return filtered_locs
+    """Filters a DataFrame, removing localizations 
+    that are not connected to other events.
+    
+    Args:
+      localizations: The DataFrame to filter.
+      event_column: The name of the column containing the events to filter.
+    
+    Returns:
+      A new DataFrame with the filtered rows.
+    """
+    
+    # Extract unique event values and their counts
+    event_counts = localizations['event'].value_counts()
+    
+    # Identify events that occur only once
+    unique_events = event_counts[event_counts == 1].index
+    
+    # Filter out rows with unique events
+    filtered_locs = localizations[~localizations['event'].isin(unique_events)]
+    
+    print('removed ', (len(localizations)-len(filtered_locs)), 'localizations.')
+    return filtered_locs
 
 def return_nearby(localization, locs_next_frame):
     '''
@@ -153,7 +174,8 @@ def return_nearby(localization, locs_next_frame):
     has_next = False
     
     locs_next = locs_next_frame.copy()
-    max_distance = localization.lpx+localization.lpy
+    
+    max_distance = (localization.lpx+localization.lpy)
     
     x_distance = (locs_next['x'].to_numpy() - localization.x)
     y_distance = (locs_next['y'].to_numpy() - localization.y)
